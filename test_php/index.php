1262227e678a25bdf9956929c460504c2463534b
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-05-04 14:27:20 +0600

    Introduction about variables

---
+++ test_php/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$student_name = 'Md.Kamrul Hasan';
+echo $student_name . "<br>";
+
+
+$student_name = 'Farzana Akter';
+echo $student_name . "<br>";
+
+$integerVar = 123;
+$floatVar = 3.67;
+$stringVar = "Bismillah";
+$boolianVar = true;
+
+echo $integerVar . "\n";
+echo $floatVar . "\n";
+echo $stringVar . "\n";
+echo $boolianVar . "\n";
+
+
+
+echo $integerVar . "<br>";
+echo $floatVar . "<br>";
+echo $stringVar . "<br>";
+echo $boolianVar . "<br>";
