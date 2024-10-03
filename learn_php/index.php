1dde5d4ce889f677613b85521354ba80923b9f2a
MD.Kamrul Hasan ⚡️ <kamrulhasan635653@gmail.com>
2024-10-03 14:15:37 +0600

    update

---
+++ learn_php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Hello php
+    <?php echo "Bismillah" ?>
+    <br>
+    <?php 
+    $food="Orange"; 
+    echo "I eat " . $food;
+
+
+
+    // MD.kamrul hasan
+    ?>
+</body>
+</html>
